eac83460ce9e011ccc8c385d5ce44232c5fe7352
fnajer <lexactr@gmail.com>
2021-04-11 18:09:38 +0300

    validate streamer's key

--- server/media_server.js
+++ server/media_server.js
@@ -1,11 +1,23 @@
 const NodeMediaServer = require('node-media-server'),
-    config = require('./config/default').rtmp_server;
- 
+    config = require('./config/default').rtmp_server,
+    User = require('./database/Schema').User;
+
 nms = new NodeMediaServer(config);
  
 nms.on('prePublish', async (id, StreamPath, args) => {
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+
+    User.findOne({stream_key: stream_key}, (err, user) => {
+        if (!err) {
+            if (!user) {
+                let session = nms.getSession(id);
+                session.reject();
+            } else {
+                // do stuff
+            }
+        }
+    });
 });
  
 const getStreamKeyFromStreamPath = (path) => {
