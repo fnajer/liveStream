6c7aa98d654c38cc79c67e3915f99da18c23a5f9
fnajer <lexactr@gmail.com>
2021-04-12 23:39:39 +0300

    fix React Router for browser

--- client/components/Root.js
+++ client/components/Root.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import LiveStreams from './LiveStreams';
 import Settings from './Settings';
