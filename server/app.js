0883a4020f61679194c394eea014ab272d5f6cb3
fnajer <lexactr@gmail.com>
2021-04-12 13:06:38 +0300

    add user route

--- server/app.js
+++ server/app.js
@@ -10,10 +10,8 @@ const express = require('express'),
     port = 3333,
     app = express(),
     passport = require('./auth/passport'),
-    node_media_server = require('./media_server');
-
-app.use(passport.initialize());
-app.use(passport.session());
+    node_media_server = require('./media_server'),
+    thumbnail_generator = require('./cron/thumbnails');
 
 mongoose.connect('mongodb://127.0.0.1/nodeStream' , { useNewUrlParser: true });
 
@@ -21,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 app.use(express.static('public'));
 app.use(flash());
+
 app.use(require('cookie-parser')());
 app.use(bodyParse.urlencoded({extended: true}));
 app.use(bodyParse.json({extended: true}));
@@ -32,17 +31,22 @@ app.use(Session({
     secret: config.server.secret,
     maxAge : Date().now + (60 * 1000 * 30),
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
 }));
 
-app.get('*', middleware.ensureLoggedIn(), (req, res) => {
-    res.render('index');
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/login', require('./routes/login'));
 app.use('/register', require('./routes/register'));
 app.use('/streams', require('./routes/streams'));
 app.use('/settings', require('./routes/settings'));
+app.use('/user', require('./routes/user'));
+
+app.get('*', middleware.ensureLoggedIn(), (req, res) => {
+    res.render('index');
+});
 
 app.listen(port, () => console.log(`App listening on ${port}!`));
 node_media_server.run();
+thumbnail_generator.start();
