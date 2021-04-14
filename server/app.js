7a46752a85a2c23db2d66f8f4668770e5083ccec
fnajer <lexactr@gmail.com>
2021-04-14 16:48:50 +0300

    fix app configs and remove old dependencies

--- server/app.js
+++ server/app.js
@@ -1,19 +1,18 @@
 const express = require('express'),
     Session = require('express-session'),
-    bodyParse = require('body-parser'),
     mongoose = require('mongoose'),
     path = require('path'),
     middleware = require('connect-ensure-login'),
     FileStore = require('session-file-store')(Session),
     config = require('./config/default'),
     flash = require('connect-flash'),
-    port = 3333,
+    port = config.server.port,
     app = express(),
     passport = require('./auth/passport'),
     node_media_server = require('./media_server'),
     thumbnail_generator = require('./cron/thumbnails');
 
-mongoose.connect('mongodb://127.0.0.1/nodeStream' , { useNewUrlParser: true });
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
@@ -21,18 +20,20 @@ app.use(express.static('public'));
 app.use('/thumbnails', express.static('server/thumbnails'));
 app.use(flash());
 
-app.use(require('cookie-parser')());
-app.use(bodyParse.urlencoded({extended: true}));
-app.use(bodyParse.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({extended: true}));
 
 app.use(Session({
     store: new FileStore({
         path : './server/sessions'
     }),
+    cookie: {
+        httpOnly: false,
+    },
     secret: config.server.secret,
-    maxAge : Date().now + (60 * 1000 * 30),
+    maxAge : Date.now() + (60 * 1000 * 30),
     resave: true,
-    saveUninitialized: false,
+    saveUninitialized: true,
 }));
 
 app.use(passport.initialize());
@@ -49,7 +50,8 @@ app.get('/logout', (req, res) => {
     res.redirect('/login');
 });
 
-app.get('*', middleware.ensureLoggedIn(), (req, res) => {
+
+app.get('*', middleware.ensureLoggedIn('/login'), (req, res) => {
     res.render('index');
 });
 
