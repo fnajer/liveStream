3680b2a678f8db4317261576f61d8097b9681990
fnajer <lexactr@gmail.com>
2021-04-15 02:43:54 +0300

    fix baseUrl path for client

--- client/config/default.js
+++ client/config/default.js
@@ -1,10 +1,10 @@
 const config = {
+  server: {
+    // baseURL: process.env.BASEURL || 'http://127.0.0.1'
+    // TODO: set ports and BASEURL on the client with webpack prod configs
+    baseURL: 'https://fnajer-livestream.zeet.app'
+  },
   rtmp_server: {
-    server: {
-        // baseURL: process.env.BASEURL || 'http://127.0.0.1'
-        // TODO: set ports and BASEURL on the client with webpack prod configs
-        baseURL: 'https://node--stream.herokuapp.com/'
-    },
     http: {
         // port: process.env.PORT_OUT || 8888,
         port: 80,
