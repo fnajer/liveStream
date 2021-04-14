e9372b7f5c4a5c6db9383442a9de4bca30ec5b83
fnajer <lexactr@gmail.com>
2021-04-14 21:45:11 +0300

    temporary fix client configs

--- client/config/default.js
+++ client/config/default.js
@@ -1,10 +1,13 @@
 const config = {
   rtmp_server: {
     server: {
-        baseURL: process.env.BASEURL || 'http://127.0.0.1'
+        // baseURL: process.env.BASEURL || 'http://127.0.0.1'
+        // TODO: set ports and BASEURL on the client with webpack prod configs
+        baseURL: 'https://node--stream.herokuapp.com/'
     },
     http: {
-        port: process.env.PORT_OUT || 8888,
+        // port: process.env.PORT_OUT || 8888,
+        port: 80,
     },
   }
 };
